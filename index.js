ffa2a6ecbab2cfbeefb264526f8c52a7e5e5c837
Remi Lapointe <remi.lapointe@gmail.com>
2017-07-07 23:23:38 +0200

    Adding some weather parameters and output in FRench

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const argv = require('yargs').argv;
 
 let apiKey = '75685783b30a2be1f1301bb83b2f0784';
 let city = argv.c || 'nantes';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+let lang = 'us';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=${lang}`
 
 request(url, function (err, response, body) {
   if(err){
@@ -11,6 +12,8 @@ request(url, function (err, response, body) {
   } else {
     let weather = JSON.parse(body)
     let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    message = `La température est de ${weather.main.temp} degrés à ${weather.name}, la pression atmosphérique est de ${weather.main.pressure} hPa`;
+	message += `\navec ${weather.clouds.all}% de nuages et un vent de ${weather.wind.speed} m/s`
     console.log(message);
   }
 });
